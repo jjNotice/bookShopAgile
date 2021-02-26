c93b38dded7617d894697ff18fd2b9c5d8dea783
Adaku Favour <P17511375@my365.dmu.ac.uk>
2021-02-26 18:09:04 +0000

    Creating properties for my table design

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Shipped { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string BookTitle { get; set; }
+        public int OrderLine { get; set; }
+        public int OrderID { get; set; }
     }
 }
