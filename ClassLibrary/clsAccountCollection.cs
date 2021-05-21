6d6e5961b731c3eee01d011223dc510b847d32bd
Jake Johnson <P2519388@my365.dmu.ac.uk>
2021-05-21 10:52:24 +0100

    another juan

---
+++ ClassLibrary/clsAccountCollection.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using ClassLibrary;
+using System.Collections.Generic;
+
+namespace AccountCollection
+
+{
+
+    public class clsAccountCollection
+
+    {
+        List<clsAccount> mAccountList = new List<clsAccount>();
+        clsAccount mThisAccount = new clsAccount();
+
+        public clsAccountCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblAccount_SelectAll");
+            PopulateArray(DB);
+        }
+
+        public List<clsAccount> AccountList
+        {
+            get
+            {
+                return mAccountList;
+            }
+            set
+            {
+                mAccountList = value;
+            }
+        }
+
+        public int count
+        {
+            get
+            {
+                return mAccountList.Count;
+            }
+            set
+
+            { 
+
+            }
+
+        }
+
+        public clsAccount ThisAccount
+
+        {
+
+            get
+
+            {
+
+                return mThisAccount;
+
+            }
+
+            set
+
+            {
+
+                mThisAccount = value;
+
+            }
+
+        }
+
+
+
+        public int Add()
+
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustFName", mThisAccount.CustFName);
+            DB.AddParameter("@CustSName", mThisAccount.CustSName);
+            DB.AddParameter("@CustAddress", mThisAccount.CustAddress);
+            DB.AddParameter("@CustPhone", mThisAccount.CustPhone);
+            DB.AddParameter("@JoinDate", mThisAccount.JoinDate);
+            DB.AddParameter("@NewsletterSub", mThisAccount.NewsLetterSub);
+            return DB.Execute("sproc_tblAccount_Insert");
+        }
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.AddParameter("@CustID", mThisAccount.CustID);
+            DB.AddParameter("@CustFName", mThisAccount.CustFName);
+            DB.AddParameter("@CustSName", mThisAccount.CustSName);
+            DB.AddParameter("@CustAddress", mThisAccount.CustAddress);
+            DB.AddParameter("@CustPhone", mThisAccount.CustPhone);
+            DB.AddParameter("@JoinDate", mThisAccount.JoinDate);
+            DB.AddParameter("@NewsletterSub", mThisAccount.NewsLetterSub);
+            DB.Execute("sproc_tblAccount_Update");
+        }
+
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.AddParameter("@CustID", mThisAccount.CustID);
+            DB.Execute("sproc_tblAccount_Delete");
+        }
+
+        public void ReportByCustAddress(string CustAddress)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustAddress", CustAddress);
+            DB.Execute("sproc_tblAccount_FilterByCustAddress");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+
+        {
+            Int32 Index = 0;
+            Int32 RecordCount;
+            RecordCount = DB.Count;
+            mAccountList = new List<clsAccount>();
+            while (Index < RecordCount)
+            {
+                clsAccount aAccount = new clsAccount();
+                aAccount.CustID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustID"]);
+                aAccount.CustFName = Convert.ToString(DB.DataTable.Rows[Index]["CustFName"]);
+                aAccount.CustSName = Convert.ToString(DB.DataTable.Rows[Index]["CustSName"]);
+                aAccount.CustPhone = Convert.ToInt32(DB.DataTable.Rows[Index]["CustPhone"]);
+                aAccount.CustAddress = Convert.ToString(DB.DataTable.Rows[Index]["CustAddress"]);
+                aAccount.NewsLetterSub = Convert.ToBoolean(DB.DataTable.Rows[Index]["NewsLetterSub"]);
+                aAccount.JoinDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["JoinDate"]);
+                mAccountList.Add(aAccount);
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing4/tstAccountCollection.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountCollection;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace Testing4
+
+{
+
+    [TestClass]
+
+    public class tstAccountCollection
+
+    {
+
+        [TestMethod]
+
+        public void InstanceOK()
+
+        {
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            Assert.IsNotNull(allAccount);
+        }
+
+        [TestMethod]
+        public void AccountListOK()
+
+        {
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            List<clsAccount> TestList = new List<clsAccount>();
+
+            clsAccount TestItem = new clsAccount();
+
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jim";
+            TestItem.CustSName = "Johnson";
+            TestItem.CustAddress = "NN3 9GA";
+            TestItem.CustPhone = 0791231231;
+            TestItem.CustID = 2;
+
+            TestList.Add(TestItem);
+
+            allAccount.AccountList = TestList;
+
+            Assert.AreEqual(allAccount.AccountList, TestList);
+
+        }
+
+        [TestMethod]
+
+        public void ThisAccountPropertyOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            clsAccount TestAccount = new clsAccount();
+
+            TestAccount.NewsLetterSub = true;
+            TestAccount.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestAccount.CustFName = "Jim";
+            TestAccount.CustSName = "Johnson";
+            TestAccount.CustAddress = "NN3 9GA";
+            TestAccount.CustPhone = 0791231231;
+            TestAccount.CustID = 2;
+
+            allAccount.ThisAccount = TestAccount;
+
+            Assert.AreEqual(allAccount.ThisAccount, TestAccount);
+        }
+
+        [TestMethod]
+
+        public void ListAndCountOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            List<clsAccount> TestList = new List<clsAccount>();
+
+            clsAccount TestItem = new clsAccount();
+
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jim";
+            TestItem.CustSName = "Johnson";
+            TestItem.CustAddress = "NN3 9GA";
+            TestItem.CustPhone = 0791231231;
+            TestItem.CustID = 2;
+
+            TestList.Add(TestItem);
+
+            allAccount.AccountList = TestList;
+
+            Assert.AreEqual(allAccount.count, TestList.Count);
+
+        }
+
+        [TestMethod]
+
+        public void AddMethodOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            clsAccount TestItem = new clsAccount();
+
+            Int32 PrimaryKey = 0;
+
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jim";
+            TestItem.CustSName = "Johnson";
+            TestItem.CustAddress = "NN3 9GA";
+            TestItem.CustPhone = 0791231231;
+            TestItem.CustID = 2;
+
+            allAccount.ThisAccount = TestItem;
+
+            PrimaryKey = allAccount.Add();
+
+            TestItem.CustID = PrimaryKey;
+
+            allAccount.ThisAccount.Find(PrimaryKey);
+
+            Assert.AreEqual(allAccount.ThisAccount, TestItem);
+
+        }
+
+
+        [TestMethod]
+
+        public void UpdateMethodOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            clsAccount TestItem = new clsAccount();
+
+            int PrimaryKey = 80;
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jim";
+            TestItem.CustSName = "Johnson";
+            TestItem.CustAddress = "NN3 9GA";
+            TestItem.CustPhone = 0791231231;
+            TestItem.CustID = 2;
+
+            allAccount.ThisAccount = TestItem;
+
+            PrimaryKey = allAccount.Add();
+
+            TestItem.CustID = PrimaryKey;
+
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jennifer";
+            TestItem.CustSName = "Carnel";
+            TestItem.CustAddress = "LE3 0DS";
+            TestItem.CustPhone = 0793213212;
+            TestItem.CustID = 3;
+
+            allAccount.ThisAccount = TestItem;
+
+            allAccount.Update();
+
+            allAccount.ThisAccount.Find(PrimaryKey);
+
+            Assert.AreEqual(allAccount.ThisAccount, TestItem);
+        }
+
+        [TestMethod]
+
+        public void DeleteMethodOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            clsAccount TestItem = new clsAccount();
+
+            Int32 PrimaryKey = 0;
+
+            TestItem.NewsLetterSub = true;
+            TestItem.JoinDate = Convert.ToDateTime("2011-01-01");
+            TestItem.CustFName = "Jim";
+            TestItem.CustSName = "Johnson";
+            TestItem.CustAddress = "NN39GA";
+            TestItem.CustPhone = 0791231231;
+            TestItem.CustID = 2;
+
+            allAccount.ThisAccount = TestItem;
+
+            PrimaryKey = allAccount.Add();
+
+            TestItem.CustID = PrimaryKey;
+
+            allAccount.ThisAccount.Find(PrimaryKey);
+
+            allAccount.Delete();
+
+            Boolean Found = allAccount.ThisAccount.Find(PrimaryKey);
+
+            Assert.IsFalse(Found);
+        }
+
+
+        [TestMethod]
+
+        public void ReportByCustAddressMethodOK()
+
+        {
+
+            clsAccountCollection allAccount = new clsAccountCollection();
+
+            clsAccountCollection filteredAccount = new clsAccountCollection();
+
+            filteredAccount.ReportByCustAddress("");
+
+            Assert.AreEqual(allAccount.count, filteredAccount.count);
+        }
+
+        [TestMethod]
+
+        public void ReportByCustAddressNoneFound()
+
+        {
+
+            clsAccountCollection filteredAccount = new clsAccountCollection();
+
+            filteredAccount.ReportByCustAddress("yyy yyy");
+
+            Assert.AreEqual(0, filteredAccount.count);
+        }
+
+        [TestMethod]
+
+        public void ReportByCustAddressTestDataFound()
+
+        {
+
+            clsAccountCollection FilteredAccount = new clsAccountCollection();
+            Boolean OK = true;
+            FilteredAccount.ReportByCustAddress("NN39GA");
+            if (FilteredAccount.count == 2)
+            {
+                if (FilteredAccount.AccountList[0].CustID != 2)
+                {
+                    OK = false;
+                }
+                if (FilteredAccount.AccountList[1].CustID != 31)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+    }
+}
